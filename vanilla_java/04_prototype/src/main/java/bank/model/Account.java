767a568c39fb7168c6df6deec7514cad005202b0
danijelradakovic <danijelradakovic1996@gmail.com>
2019-10-19 22:26:50 +0200

    Added comments in Prototype and Builder patterns

--- vanilla_java/04_prototype/src/main/java/bank/model/Account.java
+++ vanilla_java/04_prototype/src/main/java/bank/model/Account.java
@@ -1,5 +1,16 @@
 package bank.model;
+/*
+Prototype Pattern
 
+Specify the kind of objects to create using a prototypical instance, and create new
+objects by copying this prototype. - GOF Design Patterns
+
+It is determined by a prototypical instance. In the
+enterprise application, object creation is costly in terms of creating and initializing
+the initial properties of objects. If such a type of object is already in your hand, then
+you go for the prototype pattern; you just copy an existing similar object instead of
+creating it, which is time-consuming.
+ */
 public abstract class Account implements Cloneable {
 
     String number;
--- vanilla_java/05_builder/src/main/java/bank/builder/Builder.java
+++ vanilla_java/05_builder/src/main/java/bank/builder/Builder.java
@@ -1,7 +1,23 @@
 package bank.builder;
 
 import java.time.LocalDate;
+/*
+Builder Pattern
 
+Separate the construction of a complex object from its representation so that the
+same construction process can create different representations. - GOF Design
+Patterns
+
+The Builder design pattern is used to construct a complex object step by step, and
+finally it will return the complete object. The logic and process of object creation
+should be generic so that you can use it to create different concrete implementations
+of the same object type. This pattern simplifies the construction of complex objects
+and it hides the details of the object's construction from the client caller code. When
+you are using this pattern, remember you have to build it one step at a time, which
+means you have to break the object construction login into multiple phases, unlike
+other patterns, such as the abstract factory and the factory method pattern, which the
+object is built in a single step.
+*/
 public interface Builder<ConcreteBuilder extends Builder, Product> {
 
     ConcreteBuilder setPersonalID(String personalID);
